7cfdd0ec46f97620f94e12ec89a7ef82ee2c7d11
Nathan Pham <nathanpham.me@gmail.com>
2021-07-19 13:49:11 -0700

    create renderer

--- js/classes/Sketch.js
+++ js/classes/Sketch.js
@@ -1,7 +1,22 @@
 import * as THREE from "https://esm.sh/three"
 
 export default class Sketch {
-    constructor() {
-        console.log("activated")
+    constructor({container}={}) {
+        this.container = container
+        this.dimensions = {width: this.container.offsetWidth, height: this.container.offsetHeight}
+
+        this.initialize()
+    }
+
+    initialize() {
+        this.renderer = new THREE.WebGLRenderer({antialias: true, alpha: true})
+
+        this.renderer.shadowMap.enabled = true
+        this.renderer.shadowMap.type = THREE.PCFSoftShadowMap
+
+        this.renderer.setSize(this.dimensions.width, this.dimensions.height)
+        this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+        this.container.appendChild(this.renderer.domElement)
     }
 }
