a082b90c576f8bbf88ec399525514626cd305906
Nathan Pham <nathanpham.me@gmail.com>
2021-07-19 14:48:15 -0700

    create plane

--- js/app.js
+++ js/app.js
@@ -1,15 +1,17 @@
 import Sketch from "./classes/Sketch.js"
 
-import DirectionalLight from "./classes/objects/DirectionalLight.js"
-import AmbientLight from "./classes/objects/AmbientLight.js"
-import Skybox from "./classes/objects/Skybox/Skybox.js"
+import DirectionalLight from "./classes/objects/light/DirectionalLight.js"
+import AmbientLight from "./classes/objects/light/AmbientLight.js"
+import Skybox from "./classes/objects/skybox/Skybox.js"
+import Plane from "./classes/objects/Plane.js"
 
 const sketch = new Sketch({container: document.getElementById("webgl__container"), controls: true})
-sketch.add(new DirectionalLight())
-sketch.add(new AmbientLight())
 
 sketch.scene.background = (new Skybox()).object
 
-console.log(sketch)
+sketch.add(new DirectionalLight())
+sketch.add(new AmbientLight())
+sketch.add(new Plane())
+
 
 sketch.render()
---
+++ js/classes/objects/Plane.js
@@ -0,0 +1,19 @@
+import * as THREE from "https://esm.sh/three"
+
+export default class Plane { 
+    constructor() {
+        this.object = this.createPlane()
+    }
+
+    createPlane() {
+        this.geometry = new THREE.PlaneGeometry(100, 100, 1, 1)
+        this.material = new THREE.MeshBasicMaterial({color: 0xffffff})
+
+        const plane = new THREE.Mesh(this.geometry, this.material)
+        plane.rotation.x = -Math.PI / 2
+        plane.receiveShadow = true
+        plane.castShadow = true
+
+        return plane
+    }
+}
